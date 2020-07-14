04765ee8d2bddc66ba079868333f08a0fca3e27c
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-14 09:58:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by KPS on 7/14/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<String> list_Strings = new ArrayList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        System.out.println(list_Strings);
+    }
+}
